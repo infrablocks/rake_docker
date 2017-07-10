daa3f867db00c2634758ef85ca549ca3983f54eb
Toby Clemson <tobyclemson@gmail.com>
2017-07-10 20:11:44 +0100

    Only evaluate registry_id on call in ECR authentication strategy.

--- lib/rake_docker/authentication/ecr.rb
+++ lib/rake_docker/authentication/ecr.rb
@@ -5,16 +5,12 @@ module RakeDocker
   module Authentication
     class ECR
       def initialize &block
-        config = OpenStruct.new(
+        @config = OpenStruct.new(
             region: nil,
             registry_id: nil)
-        block.call(config)
+        block.call(@config)
 
-        @ecr_client = Aws::ECR::Client.new(region: config.region)
-
-        @registry_id = config.registry_id.respond_to?(:call) ?
-            config.registry_id.call :
-            config.registry_id
+        @ecr_client = Aws::ECR::Client.new(region: @config.region)
       end
 
       def arity
@@ -22,8 +18,12 @@ module RakeDocker
       end
 
       def call
+        registry_id = @config.registry_id.respond_to?(:call) ?
+            @config.registry_id.call :
+            @config.registry_id
+
         token_response = @ecr_client.get_authorization_token(
-            registry_ids: [@registry_id])
+            registry_ids: [registry_id])
         token_data = token_response.authorization_data[0]
         proxy_endpoint = token_data.proxy_endpoint
         email = 'none'

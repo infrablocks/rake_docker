3b21315de36fb2084534d2fc63eeeb810e8852ea
Toby Clemson <tobyclemson@gmail.com>
2017-08-06 19:47:43 +0100

    Add define_image_tasks helper.

--- lib/rake_docker.rb
+++ lib/rake_docker.rb
@@ -3,4 +3,7 @@ require 'rake_docker/tasks'
 require 'rake_docker/authentication'
 
 module RakeDocker
+  def self.define_image_tasks(&block)
+    RakeDocker::Tasks::All.new(&block)
+  end
 end
--- spec/rake_docker_spec.rb
+++ spec/rake_docker_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe RakeDocker do
   it 'has a version number' do
     expect(RakeDocker::VERSION).not_to be nil
   end
+
+  context 'define_image_tasks' do
+    context 'when instantiating RakeDocker::Tasks::All' do
+      it 'passes the provided block' do
+        block = lambda do |t|
+          t.image_name = 'nginx'
+          t.repository_name = 'my-org/nginx'
+          t.repository_url = '123.dkr.ecr.eu-west-2.amazonaws.com/my-org/nginx'
+
+          t.work_directory = 'build'
+
+          t.tags = ['latest']
+        end
+
+        expect(RakeDocker::Tasks::All)
+            .to(receive(:new) do |*_, &passed_block|
+              expect(passed_block).to(eq(block))
+            end)
+
+        RakeDocker.define_image_tasks(&block)
+      end
+    end
+  end
 end
